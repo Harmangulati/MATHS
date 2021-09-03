20f5f8cd1de782d961553d21b8509d7ca4b5483f
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2021-09-02 19:34:29 -0700

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,4 +1,4 @@
-function addUser(){
+function adduser(){
     player1_name = document.getElementById("player1_name_input").value;
     player2_name = document.getElementById("player2_name_input").value;   
     
--- game_page.js
+++ game_page.js
@@ -15,25 +15,36 @@ document.getElementById("player_question").innerHTML="Question turn- "+player1_n
 document.getElementById("player_answer").innerHTML="Answer turn- "+player2_name;
 
 function send(){
-  get_word=document.getElementById("word").value;
-  word=get_word.toLowerCase();
-  console.log("word in lowercase ="+word);
-  charAt1=word.charAt(1);
-  console.log(charAt1);
-  length_divide_2=Math.floor(word.length/2);
-  charAt2=word.charAt(length_divide_2);
-  console.log(charAt2);
-  length_minus_1=word.length - 1;
-charAt3=word.charAt(length_minus_1);
-console.log(charAt3);
-remove_charAt1=word.replace(charAt1,"_");
-remove_charAt2=remove_charAt1.replace(charAt2,"_");
-remove_charAt3=remove_charAt2.replace(charAt3,"_");
-console.log(remove_charAt3);
-question_word="<h4 id='word_display'>Q : "+remove_charAt3+"</h4>";
-input_box="<br> Answer: <input type='text'id='input_checkbox'> ";
-check_button="<br> <br> <button class='btn btn-info' onclick='check()'>Check</button>";
-row= question_word+input_box+check_button;
-document.getElementById("output").innerHTML=row;
-document.getElementById("word").value=" ";
+  number1 = document.getElementById("Num1").value;
+ number2 = document.getElementById("Num2").value;
+ actual_answer = parseInt(number1) * parseInt(number2);
+ question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+ input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+ check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button";
+ row = question_number + input_box + check_button;
+ document.getElementById("output").innerHTML= row;
+ document.getElementById('Num1').value=""; 
+ document.getElementById('Num2').value="";
+}
+function check(){
+get_answer = document.getElementById("input_check_box").value;
+if(get_answer == actual_answer)
+{
+ if(answer_turn == "player1")
+ document.getElementById("player1_score").innerHTML= update_player1_score; 
+}
+ else
+ {
+   update_player2_score = player2_score +1;
+   document.getElementById("player2_score").innerHTML = update_player2_score;
+ } 
+if(question_turn== "player1")
+{
+ question_turn = "player2"
+ document.getElementById("player_question").innerHTML = "Question Turn-" + player2_name;
+}
+else{
+ question_turn = "player1"
+ document.getElementById("player_question").innerHTML = "Question Turn -"+player1_name; 
+}
 }
